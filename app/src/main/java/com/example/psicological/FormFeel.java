c775bba5fba25844291ec08afff4242357ccaee4
mainc <mainc@MAKINAA>
2023-09-27 19:18:18 -0300

    1.1.1 animacion de botones personalizados

--- app/src/main/java/com/example/psicological/FormFeel.java
+++ app/src/main/java/com/example/psicological/FormFeel.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
+
+import android.widget.ImageButton;
+import android.widget.Switch;
 
 public class FormFeel extends AppCompatActivity {
 
@@ -14,11 +17,25 @@ public class FormFeel extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_feel);
 
-        Button volverbtn = (Button) findViewById(R.id.btnVolver);
-        volverbtn.setOnClickListener(new View.OnClickListener() {
+        ImageButton volverbtn = (ImageButton) findViewById(R.id.btnVolver);
+        volverbtn.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View view) {
-                startActivity(new Intent(FormFeel.this, MainActivity.class));
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+
+                        volverbtn.setImageResource(R.drawable.volver_pressed);
+                        return true;
+
+                    case MotionEvent.ACTION_UP:
+
+                        volverbtn.setImageResource(R.drawable.volver_static);
+                        startActivity(new Intent(FormFeel.this, MainActivity.class));
+                        return true;
+
+                    default:
+                        return false;
+                }
             }
         });
     }
