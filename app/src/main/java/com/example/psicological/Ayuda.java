6a5450af6163442c5866eec5f7c3617f195bc426
mainc <mainc@MAKINAA>
2023-09-28 21:36:13 -0300

    1.1.2 switch funcionales

--- app/src/main/java/com/example/psicological/Ayuda.java
+++ app/src/main/java/com/example/psicological/Ayuda.java
@@ -1,10 +1,24 @@
 package com.example.psicological;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.Collections;
 
 public class Ayuda extends AppCompatActivity {
 
@@ -12,11 +26,22 @@ public class Ayuda extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ayuda);
-        String[] opciones = {"Opción 1", "Opción 2", "Opción 3"};
+        String[] opciones = null;
+        Intent intent = getIntent();
+        if (intent != null) {
+            String[] datoRecibido = intent.getStringArrayExtra("clave");
+
+            if (datoRecibido != null) {
+                opciones=datoRecibido;
+            }
+        }
+        //String[] opciones = {"Opción 1", "Opción 2", "Opción 3"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, opciones);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        Spinner spinner = findViewById(R.id.spinner); // Reemplaza "spinner" con el ID de tu Spinner en el archivo XML
+        Spinner spinner = findViewById(R.id.spinner);
         spinner.setAdapter(adapter);
+
+
     }
 
 }
