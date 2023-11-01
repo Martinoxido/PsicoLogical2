fb26d565ffcb9cf0343b89a47154eb01510a0a55
mainc <mainc@MAKINAA>
2023-11-01 20:44:55 -0300

    Ubicacion en tiempo real

--- app/src/main/java/com/example/psicological/Maps.java
+++ app/src/main/java/com/example/psicological/Maps.java
@@ -1,24 +1,16 @@
 package com.example.psicological;
 
 
-import android.content.Context;
+
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.location.LocationRequest;
-import android.os.Build;
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-
 import android.preference.PreferenceManager;
-import android.util.Log;
 import android.widget.TextView;
-
 import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
@@ -27,13 +19,8 @@ import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.Marker;
 import org.osmdroid.views.overlay.Polyline;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
-
 import android.Manifest;
-import android.widget.Toast;
-
 import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 
@@ -42,9 +29,10 @@ public class Maps extends AppCompatActivity {
     private MapView mapView;
     private MyLocationNewOverlay myLocationOverlay;
     private FusedLocationProviderClient fusedLocationClient;
-    private LocationCallback locationCallback;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
     Marker MiMarker;
+    IMapController mapController;
+    Polyline polyline = new Polyline();
 
 
     @Override
@@ -74,18 +62,14 @@ public class Maps extends AppCompatActivity {
         mapView.setMultiTouchControls(true);
 
         // Verifica si la capa de ubicación ya se ha agregado
-        if (myLocationOverlay == null) {
-            // Si no se ha agregado, crea una nueva instancia de MyLocationNewOverlay
-            myLocationOverlay = new MyLocationNewOverlay(mapView);
-            myLocationOverlay.enableFollowLocation();
-            // Agrega la capa al mapa
 
-        }
+        ;
 
 
         //Coordenadas de Santiago, Chile
-        double santiagoLatitude = -33.4489;
-        double santiagoLongitude = -70.6693;
+        double santiagoLatitude = -33.44915;
+        double santiagoLongitude = -70.66242;
+
         //Coordenadas de Valparaíso, Chile
         double valparaisoLatitude = -33.4989778;
         double valparaisoLongitude = -70.6180072;
@@ -96,16 +80,11 @@ public class Maps extends AppCompatActivity {
         Marker santiagoMarker = new Marker(mapView);
         santiagoMarker.setPosition(santiagoPoint);
         santiagoMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
-        santiagoMarker.setTitle("Santiago, Chile");
-        santiagoMarker.setSnippet("Capital de Chile");
-        Marker valparaisoMarker = new Marker(mapView);
-        valparaisoMarker.setPosition(valparaisoPoint);
-        valparaisoMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
-        valparaisoMarker.setTitle("Valparaíso, Chile");
-        valparaisoMarker.setSnippet("Puerto principal de Chile");
+        santiagoMarker.setTitle("IP/CFT Santo Tomás");
+        santiagoMarker.setSnippet("Santiago");
+
         //Agregar marcadores al mapa
         mapView.getOverlays().add(santiagoMarker);
-        mapView.getOverlays().add(valparaisoMarker);
         // Verificar y solicitar permisos si es necesario
         if (checkLocationPermission()) {
             requestLocation();
@@ -113,22 +92,19 @@ public class Maps extends AppCompatActivity {
 
 
         //Crear y agregar la línea entre Santiago y Valparaíso
-        Polyline polyline = new Polyline();
-        polyline.addPoint(santiagoPoint);
-        polyline.addPoint(valparaisoPoint);
-        polyline.setColor(0xFF0000FF); // Color de la línea (azul en formato ARGB)
-        polyline.setWidth(5); // Ancho de la línea en píxeles
-        //Agrega la Linea al Mapa
+
+
         mapView.getOverlayManager().add(polyline);
         //Calcular la distancia entre Santiago y Valparaíso
         double distance = CalcularDistancia.CalcularDistancia(santiagoPoint, valparaisoPoint);
         TextView distanceTextView = findViewById(R.id.distanceTextView);
-        distanceTextView.setText("Distancia entre Santiago y Valparaíso: " +
+        distanceTextView.setText("Distancia entre Instuto y Tu Ubicacion: " +
                 String.format("%.2f", distance) + " km");
         // Centrar el mapa en Santiago, Chile
-        IMapController mapController = mapView.getController();
+        mapController = mapView.getController();
         mapController.setCenter(santiagoPoint);
-        mapController.setZoom(14); // Puedes ajustar el nivel de zoom según sea necesario
+        mapController.setZoom(14);
+         // Puedes ajustar el nivel de zoom según sea necesario
     }
 
     private boolean checkLocationPermission() {
@@ -175,6 +151,20 @@ public class Maps extends AppCompatActivity {
                             MiMarker.setTitle("Mi Ubicacion");
                             MiMarker.setSnippet("estas aquí");
                             mapView.getOverlays().add(MiMarker);
+                            mapController.setCenter(MiPoint);
+                            mapController.setZoom(18);
+                            TextView distanceTextView = findViewById(R.id.distanceTextView);
+                            double santiagoLatitude = -33.44915;
+                            double santiagoLongitude = -70.66242;
+
+                            GeoPoint santiagoPoint1 = new GeoPoint(santiagoLatitude, santiagoLongitude);
+                            double distance = CalcularDistancia.CalcularDistancia(santiagoPoint1,MiPoint);
+                            distanceTextView.setText("Distancia entre el Instituto y Tu Ubicacion: " +
+                                    String.format("%.2f", distance) + " km");
+                            polyline.addPoint(santiagoPoint1);
+                            polyline.addPoint(MiPoint);
+                            polyline.setColor(0xFF0000FF); // Color de la línea (azul en formato ARGB)
+                            polyline.setWidth(5);
                             // Haz lo que quieras con las coordenadas aquí
                         }
                     }
